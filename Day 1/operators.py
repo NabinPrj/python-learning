bbfb4802c68b87337948624dafb11c78ef271acd
NabinPrj <prajapati.nabin92@gmail.com>
2024-01-07 18:16:40 +0545

    operators and syntax

---
+++ Day 1/operators.py
@@ -0,0 +1,33 @@
+#set of operators
+# arithmetic  + - * / % **(square or exponent operator)
+# logical and or not
+# relational / Comparision 
+# membership ('in' and 'not in')
+# identity
+# assignment
+
+
+
+print( True and True)
+print (True and False)
+print ("k" in "kathmandu")
+print (2 in [1 ,2 ,3, 4])
+
+
+#identity operator
+a = 1
+b = 1
+print( a == b)
+print( b == a ) #True, because both are in same memory location
+
+a= []
+b = []
+print (a is b) #False, because list are allocated different memory location
+print (b is a) #False
+
+#Precedence and Associativity
+
+
+
+
+
---
+++ Day 1/syntax.py
@@ -0,0 +1,9 @@
+#Identifiers
+# For example: variables, function name, class name, module name
+
+
+#python statement
+#1. Generelly a single line is single python statement but sometimes a statement can be 2 multiple lines and multiple statements can occue in a same line.
+
+a = "Hello world. I'm learing python"
+    print(a)
